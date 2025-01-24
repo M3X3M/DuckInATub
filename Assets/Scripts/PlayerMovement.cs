1779d2f28eaa2b80f3e6a6d64c72875af7bf6394
Maximilian Wlasak <mwlasak@live.com>
2025-01-24 20:43:58 +0100

    simple movement demo now working

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] private int test;
-
+    [SerializeField] private Transform boyeTR, boyeTL, boyeBR, boyeBL;
+    [SerializeField] private float rotation_speed, movement_speed;
 
     // Start is called before the first frame update
     void Start()
@@ -16,6 +16,32 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 target_pos;
+
+        if (Input.GetButton("TopLeft"))
+        {
+            target_pos = boyeTL.position - transform.position;
+        }
+        else if (Input.GetButton("TopRight"))
+        {
+            target_pos = boyeTR.position - transform.position;
+        }
+        else if (Input.GetButton("BottomLeft"))
+        {
+            target_pos = boyeBL.position - transform.position;
+        }
+        else if (Input.GetButton("BottomRight"))
+        {
+            target_pos = boyeBR.position - transform.position;
+        }
+        else
+        {
+            return;
+        }
+
+        float target_angle = Mathf.Atan2(target_pos.x, target_pos.z) * Mathf.Rad2Deg;
+        Quaternion targetRotation = Quaternion.Euler(new Vector3(0, target_angle, 0));
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotation_speed * Time.deltaTime);
+        transform.position += movement_speed * Time.deltaTime * transform.forward;
     }
 }
